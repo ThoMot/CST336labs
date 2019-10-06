047aa2128217800ebb1ef9fe5ffbcea251e954fb
Certinax <s319217@oslomet.no>
2019-10-05 22:57:00 -0700

    Migrated javascript to dedicated file plus added couple of css variables

---
+++ public/lab5/js/main.js
@@ -0,0 +1,12 @@
+const API_KEY = "13831356-14c2a0af637ced29bdc287ad0";
+
+// Code for retrieving data from pixabay
+//   $.ajax({
+//     method: "GET",
+//     url: `https://pixabay.com/api/?key=${API_KEY}&q=sun`,
+//     dataType: "json",
+//     data: {},
+//     success: function(result, status) {
+//       console.log(result);
+//     }
+//   }); //ajax
