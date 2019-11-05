eb2108535065d19d840f09a9f92def086f48c820
ThoMot <thora.marie@outlook.com>
2019-11-04 21:39:51 -0800

    Finished lab 7

---
+++ lab7/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const getImages = require("../api/web-api-access");
+
+
+router.get("/", async function (req, res) {
+
+    let keyword = "";
+    let orientation = "vertical";
+
+    if(req.query.keyword !== undefined) {
+        keyword = req.query.keyword;
+    } else {
+        const defaultSearch = ["San Francisco", "New York", "Los Angeles", "Monterey"];
+        const searchNum = Math.floor(Math.random() * defaultSearch.length);
+        keyword = defaultSearch[searchNum];
+    }
+
+    if(req.query.orientation == "horizontal"){
+        orientation = "horizontal"
+    }
+
+    let parsedData = await getImages(keyword, orientation);
+    console.log(parsedData);
+    res.render("index", {"images":parsedData});
+
+
+}); //root route
+
+
+module.exports = router;
