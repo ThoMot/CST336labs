f85425a2aaf1f5ebfb878f76dad384c222112c20
Certinax <s319217@oslomet.no>
2019-11-18 21:44:57 -0800

    Endpoint support for keyword search

--- lab9/db/database.js
+++ lab9/db/database.js
@@ -9,14 +9,4 @@ function createConnection() {
   });
 }
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-connection.execute("SELECT * from l9_author", function(err, results, fields) {
-  console.log(results);
-  // console.log(fields);
-});
+module.exports = createConnection;
