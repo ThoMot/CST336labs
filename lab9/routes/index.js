92cb953569edeb75929a0ab8d15bd7ffebea296f
Thora Mothes <thora.marie@outlook.com>
2019-11-19 01:06:52 -0800

    endpoint for all author info (#41)

--- lab9/routes/index.js
+++ lab9/routes/index.js
@@ -128,6 +128,30 @@ router.post("/authorSearch", function(req, res) {
   connection.end();
 });
 
+
+router.post("/allAuthorInfo", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.author;
+  const lastname = searchParam.split(" ")[1];
+  const query = `SELECT a.dob, a.dod, a.sex, a.profession, a.country, a.portrait, a.biography, CONCAT(a.firstName,' ',a.lastName) as 'name' FROM l9_author a WHERE a.lastname LIKE ('${lastname}')`;
+    connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
+
 router.post("/categorySearch", function(req, res) {
   const connection = createConnection();
   const searchParam = req.body.category;
@@ -149,5 +173,4 @@ router.post("/categorySearch", function(req, res) {
   });
   connection.end();
 });
-
 module.exports = router;
