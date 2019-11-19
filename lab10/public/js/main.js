5846a7c3863cac1dcabe15852bbb0683459943ac
ThoMot <thora.marie@outlook.com>
2019-11-19 14:37:23 -0800

    lab10 setup

---
+++ lab10/public/js/main.js
@@ -0,0 +1,286 @@
+$(document).ready(() => {
+  listAuthors();
+  listGenders();
+  listCategories();
+
+  $("#genderSearch").on("click", function() {
+    const genderValue = $("#genderSelect").val();
+    if (genderValue !== "default") {
+      searchGender(genderValue);
+    }
+  });
+
+  // Adding eventlisteners for author in quotes
+  $("#quotesDiv").on("click", "a", function() {
+    const authorName = $(this)[0].text;
+    getAuthorInfo(authorName);
+  });
+
+  $("#keywordSearch").on("click", function() {
+    const keywordValue = $("#keyword").val();
+    if (keywordValue !== "") {
+      searchKeyword(keywordValue.trim());
+    } else {
+      $("#keyword").attr("placeholder", "Please input a search word");
+    }
+  });
+
+  $("#authorSearch").on("click", function() {
+    const authorValue = $("#authorSelect").val();
+    if (authorValue !== "default") {
+      searchAuthor(authorValue);
+    }
+  });
+
+  $("#categorySearch").on("click", function() {
+    const categoryValue = $("#categorySelect").val();
+    if (categoryValue !== "default") {
+      searchCategory(categoryValue);
+    }
+  });
+
+  $("#loginButton").on("click", function () {
+
+  });
+
+});
+
+function listAuthors() {
+  $.ajax({
+    url: "/authors",
+    method: "get",
+    contentType: "application/json",
+    dataType: "json",
+    success: function(result) {
+      result.forEach(author => {
+        $("#authorSelect").append(
+          `<option value=${author.lastName}>${author.firstName} ${author.lastName}</option>`
+        );
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function listGenders() {
+  $.ajax({
+    url: "/genders",
+    method: "get",
+    contentType: "application/json",
+    dataType: "json",
+    success: function(result) {
+      result.forEach(gender => {
+        if (gender.sex === "F") {
+          $("#genderSelect").append(
+            `<option value=${gender.sex}>Female</option>`
+          );
+        } else if (gender.sex === "M") {
+          $("#genderSelect").append(
+            `<option value=${gender.sex}>Male</option>`
+          );
+        }
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function listCategories() {
+  $.ajax({
+    url: "/categories",
+    method: "get",
+    contentType: "application/json",
+    dataType: "json",
+    success: function(result) {
+      result.forEach(category => {
+        $("#categorySelect").append(
+          `<option value=${category.category}>${category.category}</option>`
+        );
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function searchGender(genderValue) {
+  $.ajax({
+    url: "/genderSearch",
+    method: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      gender: genderValue
+    }),
+    success: function(result) {
+      $("#quotesDiv").empty();
+      result.forEach(qt => {
+        displayQuote(qt.author, qt.quote);
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function searchKeyword(keyword) {
+  $.ajax({
+    url: "/keywordSearch",
+    method: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      keyword: keyword
+    }),
+    success: function(result) {
+      $("#quotesDiv").empty();
+      if (result.error === 1) {
+        displayError(result.message);
+      } else {
+        result.forEach(qt => {
+          displayQuote(qt.author, qt.quote);
+        });
+      }
+    },
+    error: function(xhr, status) {
+      console.log(status);
+    },
+    complete: function() {}
+  });
+}
+
+function searchAuthor(authorValue) {
+  $.ajax({
+    url: "/authorSearch",
+    method: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      author: authorValue
+    }),
+    success: function(result) {
+      $("#quotesDiv").empty();
+      result.forEach(qt => {
+        displayQuote(qt.author, qt.quote);
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function getAuthorInfo(authorName) {
+  $.ajax({
+    url: "/allAuthorInfo",
+    method: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      authorName: authorName
+    }),
+    success: function(result) {
+      updateModal(result[0]);
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function searchCategory(categoryValue) {
+  $.ajax({
+    url: "/categorySearch",
+    method: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      category: categoryValue
+    }),
+    success: function(result) {
+      $("#quotesDiv").empty();
+      result.forEach(qt => {
+        displayQuote(qt.author, qt.quote);
+      });
+    },
+    error: function(xhr, status) {
+      console.log("error calling to POST router", status);
+    },
+    complete: function() {}
+  });
+}
+
+function displayQuote(author, quote) {
+  const quoteResult = `<div class="card my-2">
+            <div class="card-body">
+                <div class="card-title">
+                    Author: <a href="#" class="event" data-toggle="modal" data-target="#exampleModal">${author}</a>
+                </div>
+            <hr align="left" width="40%">
+                ${quote}
+            </div>
+        </div>`;
+  $("#quotesDiv").append(quoteResult);
+}
+
+function displayError(message) {
+  const quoteResult = `<div class="card my-2">
+      <div class="card-body">
+        ${message}
+      </div>
+    </div>`;
+  $("#quotesDiv").append(quoteResult);
+}
+
+function updateModal(author) {
+  $(".card-list").empty();
+  const {
+    name,
+    dob,
+    dod,
+    sex,
+    profession,
+    country,
+    portrait,
+    biography
+  } = author;
+
+  const birth = new Date(dob);
+  const death = new Date(dod);
+  birth.setMonth(birth.getMonth() + 1);
+  death.setMonth(death.getMonth() + 1);
+
+  $("#card-title").text(`${name}`);
+  $(".card-list").append(
+    `<li>Date of birth: ${birth.getDate() +
+      "-" +
+      birth.getMonth() +
+      "-" +
+      birth.getFullYear()}`
+  );
+  $(".card-list").append(
+    `<li>Date of death: ${death.getDate() +
+      "-" +
+      death.getMonth() +
+      "-" +
+      death.getFullYear()}`
+  );
+  $(".card-list").append(`<li>Gender: ${sex === "M" ? "Male" : "Female"}`);
+  $(".card-list").append(`<li>Profession: ${profession}`);
+  $(".card-list").append(`<li>Country: ${country}`);
+  $("#card-img").attr("src", `${portrait}`);
+  $("#card-img").attr("alt", `portrait of ${name}`);
+  $(".card-text").text(`${biography}`);
+}
--- lab10/routes/index.js
+++ lab10/routes/index.js
@@ -1,11 +1,176 @@
 const express = require("express");
 const router = express.Router();
-//const createConnection = require("../db/database");
+const createConnection = require("../db/database");
 
 router.get("/", function(req, res) {
-    res.render("index", {
-        title: "Lab 10"
-    });
+  res.render("index", {
+    title: "Lab 10 - Quotes Lookup/Editing"
+  });
 });
 
+router.get("/admin", function (req, res) {
+  res.render("admin", {
+    title: "Lab 10 - Admin page"
+  });
+});
+
+router.get("/authors", function(req, res) {
+  const connection = createConnection();
+  connection.execute("SELECT firstName, lastName FROM l9_author", function(
+    err,
+    results,
+    fields
+  ) {
+    res.json(results);
+  });
+  connection.end();
+});
+
+router.get("/genders", function(req, res) {
+  const connection = createConnection();
+
+  connection.execute("SELECT DISTINCT sex FROM l9_author", function(
+    err,
+    results,
+    fields
+  ) {
+    res.json(results);
+  });
+  connection.end();
+});
+
+router.get("/categories", function(req, res) {
+  const connection = createConnection();
+
+  connection.execute("SELECT DISTINCT category from l9_quotes", function(
+    err,
+    results,
+    fields
+  ) {
+    res.json(results);
+  });
+
+  connection.end();
+});
+
+router.post("/keywordSearch", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.keyword;
+  const words = searchParam.split(" ");
+  let searchQuery = "'%";
+  words.forEach((word, i) => {
+    searchQuery += word;
+    if (i < words.length - 1) {
+      searchQuery += " ";
+    } else {
+      searchQuery += "%'";
+    }
+  });
+  const query = `SELECT q.quote, CONCAT(a.firstName,' ',a.lastName) as 'author' FROM l9_quotes q INNER JOIN l9_author a ON a.authorId = q.authorId WHERE quote LIKE ${searchQuery};`;
+  connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
+
+router.post("/genderSearch", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.gender;
+  const query = `SELECT q.quote, CONCAT(a.firstName,' ',a.lastName) as 'author' FROM l9_quotes q INNER JOIN l9_author a ON a.authorId = q.authorId WHERE a.sex LIKE ('${searchParam}')`;
+  connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
+
+router.post("/authorSearch", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.author;
+  const query = `SELECT q.quote, CONCAT(a.firstName,' ',a.lastName) as 'author' FROM l9_quotes q INNER JOIN l9_author a ON a.authorId = q.authorId WHERE a.lastname LIKE ('${searchParam}')`;
+  connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
+
+router.post("/allAuthorInfo", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.authorName;
+  const lastname = searchParam.split(" ")[1];
+  const query = `SELECT a.dob, a.dod, a.sex, a.profession, a.country, a.portrait, a.biography, CONCAT(a.firstName,' ',a.lastName) as 'name' FROM l9_author a WHERE a.lastname LIKE ('${lastname}')`;
+  connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
+
+router.post("/categorySearch", function(req, res) {
+  const connection = createConnection();
+  const searchParam = req.body.category;
+  const query = `SELECT q.quote, CONCAT(a.firstName,' ',a.lastName) as 'author' FROM l9_quotes q INNER JOIN l9_author a ON a.authorId = q.authorId WHERE q.category LIKE ('${searchParam}')`;
+  connection.execute(query, function(err, result, fields) {
+    if (err) {
+      res.json(err);
+    }
+    if (result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          message: "No matches for given search",
+          error: 1
+        });
+      }
+    }
+  });
+  connection.end();
+});
 module.exports = router;
