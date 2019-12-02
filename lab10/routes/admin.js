6bf6e6a6942018a5b45ecdf9620ea67ea57a990c
Certinax <31922229+Certinax@users.noreply.github.com>
2019-12-02 08:22:13 +0100

    Route for authors in admin and prepping data before returning (#50)

--- lab10/routes/admin.js
+++ lab10/routes/admin.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const createConnection = require("../db/database");
+const dateConverter = require("../tools/datehandler");
 
-
-router.get("/", function (req, res) {
-    res.render("admin", {
-        title: "Lab 10 - Admin page"
-    });
+router.get("/", function(req, res, next) {
+  res.render("admin", {
+    title: "Lab 10 - Admin page"
+  });
 });
 
+router.get("/authors", function(req, res, next) {
+  const connection = createConnection();
+  connection.execute(
+    "SELECT authorId, firstName, lastName, dob, dod FROM l9_author",
+    function(err, results, fields) {
+      if (err) {
+        console.log(err);
+        res.json({
+          status: "error",
+          message: "Error retrieving authors"
+        });
+      } else {
+        results.map(author => {
+          // Formatting sql date object into YYYY-MM-DD format
+          author.dob = dateConverter(author.dob);
+          author.dod = dateConverter(author.dod);
+        });
+        res.json({
+          status: "success",
+          authors: results
+        });
+      }
+    }
+  );
+  connection.end();
+});
 
 module.exports = router;
