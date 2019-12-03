d8cd06f69f47a67ad449e901eb2ad341bf1c23f4
Certinax <31922229+Certinax@users.noreply.github.com>
2019-12-03 07:57:17 +0100

    Feature/i59 login logout page (#69)
    * Added login page and redirects
    
    * Redirect to home when modal is closed

--- lab10/app.js
+++ lab10/app.js
@@ -17,28 +17,30 @@ hbs.registerPartials(path.join(__dirname, "/views/partials"));
 // Routes
 const indexRouter = require("./routes");
 const adminRouter = require("./routes/admin");
+const loginRouter = require("./routes/login");
 
 // Route listeners
 app.use("/", indexRouter);
 app.use("/admin", adminRouter);
+app.use("/login", loginRouter);
 
 // Server startup
 function normalizePort(val) {
-    var port = parseInt(val, 10);
+  var port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 app.listen(port, function() {
-    console.log("Express server is running on port: ", port);
+  console.log("Express server is running on port: ", port);
 });
