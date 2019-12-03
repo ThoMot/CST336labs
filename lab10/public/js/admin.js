da04b5f68df5981376f4f486196475185823fd56
Certinax <s319217@oslomet.no>
2019-12-03 00:43:05 -0800

    Updating table on success

--- lab10/public/js/admin.js
+++ lab10/public/js/admin.js
@@ -112,6 +112,7 @@ function deleteAuthor(authorId) {
     }),
     success: function(result) {
       $("#deleteSuccess").html(result.message);
+      retreiveAuthors();
     },
     error: function(xhr, status) {
       $("#deleteFailed").show();
